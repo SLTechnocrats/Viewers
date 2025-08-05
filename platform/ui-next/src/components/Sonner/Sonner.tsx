75aa813f0a9b04ec1380d0df7ed7d55af0ff79e3
priyanshu-rathore-slt <priyanshu@sltechnocrats.net>
2025-08-05 17:39:08 +0530

    Update Sonner.tsx

--- platform/ui-next/src/components/Sonner/Sonner.tsx
+++ platform/ui-next/src/components/Sonner/Sonner.tsx
@@ -6,25 +6,27 @@ type ToasterProps = React.ComponentProps<typeof Sonner>;
 
 const Toaster = ({ ...props }: ToasterProps) => {
   return (
-    <Sonner
-      className="toaster group"
-      loadingIcon={<Icons.LoadingSpinner />}
-      icons={{
-        warning: <Icons.StatusWarning />,
-        info: <Icons.Info className="text-secondary-foreground" />,
-        success: <Icons.StatusSuccess />,
-        error: <Icons.StatusError />,
-      }}
-      theme="dark"
-      richColors="true"
-      toastOptions={{
-        style: {
-          width: '430px', // Set a maximum width
-          right: '8px',
-        },
-      }}
-      {...props}
-    />
+    <div>
+      <Sonner
+        className="toaster group"
+        loadingIcon={<Icons.LoadingSpinner />}
+        icons={{
+          warning: <Icons.StatusWarning />,
+          info: <Icons.Info className="text-secondary-foreground" />,
+          success: <Icons.StatusSuccess />,
+          error: <Icons.StatusError />,
+        }}
+        theme="dark"
+        richColors="true"
+        toastOptions={{
+          style: {
+            width: '430px', // Set a maximum width
+            right: '8px',
+          },
+        }}
+        {...props}
+      />
+    </div>
   );
 };
 
